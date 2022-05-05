32a7e7b38f298eabdb3816e876a47dfc05db91cf
RAHUL TANWAR <rahul@magicapps.co.in>
2022-05-05 23:59:01 +0530

    login successful

--- app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
+++ app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
@@ -1,28 +1,61 @@
 package com.errorsmasher.mytask
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.database.FirebaseDatabase
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+const val RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
-    val database = FirebaseDatabase.getInstance()
-    private var client: GoogleSignInClient? = null
+    private var mGoogleSignInClient: GoogleSignInClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myRef = database.getReference("message")
-        myRef.setValue("Hello, World!")
-    }
-
-    fun login(view: View) {
+        //
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("956918141582-bi9n0u8uqdlkpqa760r9ab0dg9dmhrdb.apps.googleusercontent.com")
             .requestEmail()
             .build()
-        client = GoogleSignIn.getClient(this, gso)
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+//            sign_in_button.visibility = View.GONE
+//            tv_name.text = acct.displayName
+//            tv_name.visibility = View.VISIBLE
+            Log.d("qqq", "done")
+        }
+    }
+
+    fun login(view: View) {
+        val signInIntent = mGoogleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.d("qqq", "done two")
+        } catch (e: ApiException) {
+            Log.d("qqq", e.toString())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+         if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
     }
 }
