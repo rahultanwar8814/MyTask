fa0c96fc72da0e433e1d6260047ccce93d0f11b2
RAHUL TANWAR <rahul@magicapps.co.in>
2022-05-06 00:09:05 +0530

    login successful code refactor

--- app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
+++ app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
@@ -2,9 +2,9 @@ package com.errorsmasher.mytask
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.errorsmasher.mytask.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -16,44 +16,44 @@ const val RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
     private var mGoogleSignInClient: GoogleSignInClient? = null
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("956918141582-bi9n0u8uqdlkpqa760r9ab0dg9dmhrdb.apps.googleusercontent.com")
             .requestEmail()
             .build()
-        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         val acct = GoogleSignIn.getLastSignedInAccount(this)
         if (acct != null) {
-//            sign_in_button.visibility = View.GONE
-//            tv_name.text = acct.displayName
-//            tv_name.visibility = View.VISIBLE
-            Log.d("qqq", "done")
+            binding.login.visibility = View.GONE
+            binding.name.visibility = View.VISIBLE
+            binding.name.text = acct.displayName
         }
-    }
-
-    fun login(view: View) {
-        val signInIntent = mGoogleSignInClient?.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        binding.login.setOnClickListener {
+            val signInIntent = mGoogleSignInClient?.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-            Log.d("qqq", "done two")
+            binding.login.visibility = View.GONE
+            binding.name.visibility = View.VISIBLE
+            binding.name.text = account.displayName
         } catch (e: ApiException) {
-            Log.d("qqq", e.toString())
+            binding.login.visibility = View.VISIBLE
+            binding.name.visibility = View.GONE
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
+        if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             handleSignInResult(task)
         }
