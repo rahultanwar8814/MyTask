b0127677dacfc2cdb0224b551b16ce0a571471e3
RAHUL TANWAR <rahul@magicapps.co.in>
2022-05-06 00:30:12 +0530

    getting live location 2

--- app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
+++ app/src/main/java/com/errorsmasher/mytask/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.errorsmasher.mytask
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.errorsmasher.mytask.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Task
 
 const val RC_SIGN_IN = 123
@@ -17,11 +23,13 @@ const val RC_SIGN_IN = 123
 class MainActivity : AppCompatActivity() {
     private var mGoogleSignInClient: GoogleSignInClient? = null
     private lateinit var binding: ActivityMainBinding
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("956918141582-bi9n0u8uqdlkpqa760r9ab0dg9dmhrdb.apps.googleusercontent.com")
             .requestEmail()
@@ -37,6 +45,9 @@ class MainActivity : AppCompatActivity() {
             val signInIntent = mGoogleSignInClient?.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
+        binding.getLoc.setOnClickListener {
+            checkLocationPermission()
+        }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -48,6 +59,27 @@ class MainActivity : AppCompatActivity() {
         } catch (e: ApiException) {
             binding.login.visibility = View.VISIBLE
             binding.name.visibility = View.GONE
+        }
+    }
+
+    fun checkLocationPermission() {
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                101)
+
+            return
+        }
+        task.addOnSuccessListener {
+            if (it != null) {
+                Toast.makeText(applicationContext,
+                    "${it.latitude} ${it.longitude}",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
